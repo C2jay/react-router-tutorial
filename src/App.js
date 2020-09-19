681831085f9a4e668805ea766f36700e6f10ea9d
C2jay <cat.crane15@gmail.com>
2020-09-19 09:57:20 +0800

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from "./components/Nav";
+import Home from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import HomePage from './pages/HomePage';
+// import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        
+        <Switch>
+
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+function About() {
+    return <h1>You must not know 'bout me.</h1>
+}
+
+
+export default About;
